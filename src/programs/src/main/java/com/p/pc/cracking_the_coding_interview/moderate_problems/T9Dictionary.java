e898b8f693492146bde86eef3879e7e79f31b887
Prince Chauda <prince.chauda@gmail.com>
2022-01-26 09:45:31 -0500

    Added more problems to T9Dictionary

--- src/programs/src/main/java/com/p/pc/cracking_the_coding_interview/moderate_problems/T9Dictionary.java
+++ src/programs/src/main/java/com/p/pc/cracking_the_coding_interview/moderate_problems/T9Dictionary.java
@@ -2,7 +2,6 @@ package com.p.pc.cracking_the_coding_interview.moderate_problems;
 
 import com.p.pc.cracking_the_coding_interview.code_library.Trie;
 import com.p.pc.cracking_the_coding_interview.code_library.TrieNode;
-import com.p.pc.graphs.TrieDataStructure;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,25 +26,64 @@ import java.util.List;
  * Approach:<br/>
  * Follow a Trie based approach to solve this problem optimally.
  *
- * Time Complexity: O(n * s) where n = length of the given num string, s = length of the longest word in dictionary
+ * Time Complexity: First a Trie needs to be created from the given dictionary and then search is happening and hence
+ * total time complexity will be O(w * l + n * l)
+ * where
+ *  w = total words in dictionary
+ *  n = length of the given num string
+ *  l = avg length of the word in dictionary
+ *
+ *  There are different flavors of this problem such that:
+ *  1. Find all words represented by the given num string
+ *  2. Find all words starting from the given num string
+ *  3. Find all words contained within the given num string
  */
 public class T9Dictionary {
 
     public static void main(String[] args) {
-        String numString = "8733";
-        String[] validWords = new String[]{"used", "tree", "true", "bat", "cat", "mat", "trees"};
+        String[] validWords = new String[]{"use", "used", "tree", "true", "bat", "cat", "mat", "trees"};
         Trie trie = new Trie(validWords);
-        List<String> validT9Words = getValidT9Words(numString, trie);
-        System.out.println(validT9Words);
+        // Find all words represented by the given num string
+        System.out.println(findAllWordsRepresentingNumString("873", trie)); // Output: [use]
+        // Find all words starting from the given num string
+        System.out.println(findAllWordsStartingFromNumString("873", trie)); // Output: [tree, trees, use, used]
+        // Find all words contained within the given num string
+        // TODO
     }
 
-    private static List<String> getValidT9Words(String numString, Trie trie) {
+    // 1. This method returns all words represented by the given num string
+    static List<String> findAllWordsRepresentingNumString(String numString, Trie trie) {
         List<String> words = new ArrayList<>();
-        getValidWords(numString, 0, "", trie.getRoot(), words);
+        findAllWordsRepresentingNumStringRecursive(numString, 0, "", trie.getRoot(), words);
         return words;
     }
 
-    private static void getValidWords(String numString, int index, String prefix, TrieNode node, List<String> result) {
+    private static void findAllWordsRepresentingNumStringRecursive(String numString, int index, String prefix, TrieNode node, List<String> result) {
+        if (index == numString.length()) {
+            if(node.terminates())
+                result.add(prefix);
+            return;
+        }
+        char n = numString.charAt(index); // pick the number character at given index
+        char[] letters = getT9Chars(n); // get all linked english characters
+        if (letters != null) {
+            for (char c : letters) {
+                TrieNode child = node.getChild(c);
+                if (child != null) {
+                    findAllWordsRepresentingNumStringRecursive(numString, index + 1, prefix + c, child, result);
+                }
+            }
+        }
+    }
+
+    // 2. This method returns all words starting from the given num string
+    static List<String> findAllWordsStartingFromNumString(String numString, Trie trie) {
+        List<String> words = new ArrayList<>();
+        findAllWordsStartingFromNumStringRecursive(numString, 0, "", trie.getRoot(), words);
+        return words;
+    }
+
+    private static void findAllWordsStartingFromNumStringRecursive(String numString, int index, String prefix, TrieNode node, List<String> result) {
         if (index == numString.length()) {
             findAllWordsStartingFromTheNode(prefix, node, result);
             return;
@@ -56,19 +94,19 @@ public class T9Dictionary {
             for (char c : letters) {
                 TrieNode child = node.getChild(c);
                 if (child != null) {
-                    getValidWords(numString, index + 1, prefix + c, child, result);
+                    findAllWordsStartingFromNumStringRecursive(numString, index + 1, prefix + c, child, result);
                 }
             }
         }
     }
 
-    private static void findAllWordsStartingFromTheNode(String prefix, TrieNode node, List<String> words) {
-        if(node.terminates()) words.add(prefix);
+    private static void findAllWordsStartingFromTheNode(String prefix, TrieNode node, List<String> result) {
+        if(node.terminates()) result.add(prefix);
         if(node.getChildren().isEmpty()) {
             return;
         }
         for(Character c: node.getChildren().keySet()) {
-            findAllWordsStartingFromTheNode(prefix + c, node.getChildren().get(c), words);
+            findAllWordsStartingFromTheNode(prefix + c, node.getChildren().get(c), result);
         }
     }
 
